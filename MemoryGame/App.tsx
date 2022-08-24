014261052ebd88bf4c96149fc2f1a39846cd5311
Julio <vanssabento@gmail.com>
2022-08-24 14:29:55 -0300

    Estruturando o game

--- MemoryGame/App.tsx
+++ MemoryGame/App.tsx
@@ -1,24 +1,91 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { Button, StatusBar, StyleSheet, Text, View } from 'react-native'
-// import { Container, Col, Row } from "react-bootstrap";
+//tentar importar css de outra pasta
+
+
+const appStyle = StyleSheet.create({
+  title: {
+    color: 'black' ,
+    textAlign: 'center',
+    fontSize: 40,
+    marginTop: 20
+  },
+  time: {
+    color: 'black' ,
+    textAlign: 'center',
+    fontSize: 60,
+    marginTop: 20
+  },
+  card: {
+    backgroundColor: 'yellow',
+    width: 80,
+    height: 80,
+    margin: 10
+  },
+  backcard: {
+    display: 'flex',
+    flex: 0,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    margin: 25
+  },
+  pair: {
+    color: 'black' ,
+    textAlign: 'center',
+    fontSize: 24,
+    margin: 10,
+    marginTop: 50
+  },
+  button: {    
+    padding: 10
+  }
+})
+
+
 
 const App = () => {
+  const toggleRuning = () => {
+    
+  }
+
   return(
     <View>
-      <Text>Jogo da Memoria</Text>
-      <Text>00:00</Text>
-      <View>
-        <View></View>
-        <View></View>
-        <View></View>
-        <View></View>
-        <View></View>
-        <View></View>
-        <View></View>
-        <View></View>
+      <Text style={appStyle.title}>Jogo da Memoria</Text>
+      <Text style={appStyle.time}>00:00</Text>
+      <View style={appStyle.backcard}>
+        <View>
+          <View style={appStyle.card}></View>  
+          <View style={appStyle.card}></View>
+          <View style={appStyle.card}></View>
+          <View style={appStyle.card}></View>
+        </View>
+        <View>
+          <View style={appStyle.card}></View>  
+          <View style={appStyle.card}></View>
+          <View style={appStyle.card}></View>
+          <View style={appStyle.card}></View>
+        </View>
+        <View>
+          <View style={appStyle.card}></View>  
+          <View style={appStyle.card}></View>
+          <View style={appStyle.card}></View>
+          <View style={appStyle.card}></View>
+        </View>
+        <View>
+          <View style={appStyle.card}></View>  
+          <View style={appStyle.card}></View>
+          <View style={appStyle.card}></View>
+          <View style={appStyle.card}></View>
+        </View>
+        
       </View>
-      <Text>Faltam 99 pares.</Text>
-      <Button title='Iniciar' />
+      <Text style={appStyle.pair}>Faltam 99 pares.</Text>
+      <View style={appStyle.button}>
+        <Button title='Iniciar'color={'#daa520'} onPress={toggleRuning} />
+      </View>
+      
     </View>
   )
 }
+
+export default App
